aedf4d13c067a26d0f24b72b7642c07c4ded4a7c
Daniel Francis <danfrancix@gmail.com>
2023-08-18 06:11:51 +0100

    Footer corrected

--- src/Pages/Hero Page/Footer.jsx
+++ src/Pages/Hero Page/Footer.jsx
@@ -3,16 +3,16 @@ import logo from "asstes/Solution Road New Logo.png"
 
 const Footer = () => {
   return (
-    <div className='bg-[#06080d] w-full h-fit'>
-        <div className='w-full flex flex-col lg:flex-row '>
+    <div className='bg-[#06080d] w-screen h-fit'>
+        <div className='w-full flex flex-col items-center justify-between'>
             <div className='lg:w-1/2 h-fit w-full'>
-                <div className='w-full flex items-center justify-between'>
-                    <div className="rounded-full border border-solid border-black bg-white bod" style={{width:"40px", height:"40px",}}>
+                <div className='w-full h-auto flex items-center justify-center'>
+                    <div className="rounded-full border border-solid border-black bg-white bod" style={{width:"100px", height:"100px",}}>
                         <img className="w-full max-w-full h-auto sm:w-6/2 md:w-full" src={logo} alt="Image description"/>  
                     </div>
                     
-                    <span className='font-poppins font-bold cursor-pointer text-sm text-yellow-500'>Solutions Road</span>
-                    <span className='font-poppins font-bold cursor-pointer text-yellow-500 text-[10px]'>Equipments & Sparks Ltd</span>
+                    <span className='font-poppins ml-4 mr-2 font-bold cursor-pointer text-lg flex  text-yellow-500'>Solutions Road</span>
+                    <span className='font-poppins font-bold cursor-pointer flex text-yellow-500 text-lg'>Equipments & Sparks Ltd</span>
                 </div>
             </div>
             <div className='w-6/2 h-fit'></div>
