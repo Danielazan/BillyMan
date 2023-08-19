6ea85e8996beb25ddfd3accd644eb193ad71380f
Daniel Francis <danfrancix@gmail.com>
2023-08-19 15:07:01 +0100

    About page finally started

--- src/Pages/About Page/About.jsx
+++ src/Pages/About Page/About.jsx
@@ -6,7 +6,7 @@ import './styles/Aboutbgbtn.css'
 const About = () => {
   return (
     <div className='w-screen flex flex-col '>
-      <div className=' hidden md:block bg-cover bg-no-repeat w-full max-w-full h-[70vh]  md:w-full' style={{ backgroundImage: `url(${Aboutbg})`, backgroundPosition:`center`}} >
+      <div className='bg-cover bg-no-repeat w-full max-w-full h-[70vh]  md:w-full' style={{ backgroundImage: `url(${Aboutbg})`, backgroundPosition:`center`}} >
         <Navbars/>
             {/* the hero side of the about page */}
           <div className='flex flex-col w-full items-center justify-center '>
@@ -34,7 +34,7 @@ const About = () => {
 
                   <div className='w-full h-fit flex items-center justify-center'>
                     <div class="spinner">
-                      <div></div>
+                      <div></div> 
                       <div></div>
                       <div></div>
                       <div></div>
