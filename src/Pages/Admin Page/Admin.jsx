29719734cdd130cc2bf8d3598cd2b29a1f53c024
Daniel Francis <danfrancix@gmail.com>
2023-08-26 18:07:34 +0100

    Equipment page almost complete

--- src/Pages/Admin Page/Admin.jsx
+++ src/Pages/Admin Page/Admin.jsx
@@ -22,7 +22,7 @@ const Admin = () => {
     setIsOpen(!isOpen);
   };
   return (
-    <div className='w-screen h-fit bg-[#081225]'>
+    <div className='w-full h-fit bg-[#081225]'>
         <Navbar/>
 
         <div className='w-full flex items-center pl-4 md:hidden'>
@@ -144,12 +144,27 @@ const Admin = () => {
                     </div>
                 </motion.div>
             </div>
+            {/* Second half of the the Admin page begins here */}
             <div  className=" w-full md:w-5/6 h-screen">
-                <div className="w-full">
+                <div className="w-full p-4">
+                <motion.div
+                    initial={{ opacity: 0, x: -100 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.8 }}
+                    className=" rounded-xl w-full"
+                >
+                    <div className="w-full h-[4rem] rounded-xl " style={{ border: '2px solid white' }}>
+                        <div className='w-full'>
+                            <h1>Hello</h1>
+                        </div>
+                    </div>
+                    <div className="w-full h-[22rem] rounded-xl mt-6" style={{ border: '2px solid white' }}></div>
+                    <div className="w-full h-[10rem] rounded-xl mt-4" style={{ border: '2px solid white' }}></div>
 
+                </motion.div>
                 </div>
             </div>
-    </div>
+        </div>
     </div>
   )
 }
